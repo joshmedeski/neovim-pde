d89a3065c42d6e82cca40bbaa975a6711f89e9bb
Josh Medeski <josh.medeski@gmail.com>
2022-11-25 22:50:15 -0600

    feat: add transparent support

--- init.lua
+++ init.lua
@@ -1,3 +1,8 @@
+-- TODO: setup transparency
+-- TODO: setup which key
+-- TODO: port all my leader.vim commands (and heck luavim)
+-- TODO: setup hammerspoon workflow
+-- TODO: add todo icons
 require "user.impatient"
 require "user.options"
 require "user.keymaps"
@@ -7,6 +12,7 @@ require "user.colorscheme"
 require "user.cmp"
 require "user.telescope"
 require "user.gitsigns"
+require "user.transparent"
 require "user.treesitter"
 require "user.autopairs"
 require "user.comment"
