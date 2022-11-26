3ea5b95fee5b431b2efcac524aa5cbfc4419d8bd
Josh Medeski <josh.medeski@gmail.com>
2022-11-26 11:46:15 -0600

    feat: update alpha

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -5,24 +5,23 @@ end
 
 local dashboard = require "alpha.themes.dashboard"
 dashboard.section.header.val = {
-  [[                               __                ]],
-  [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-  [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-  [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-  [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-  [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+  [[     ██╗ ██████╗ ███████╗██╗  ██╗    ███╗   ███╗███████╗██████╗ ███████╗███████╗██╗  ██╗██╗]],
+  [[     ██║██╔═══██╗██╔════╝██║  ██║    ████╗ ████║██╔════╝██╔══██╗██╔════╝██╔════╝██║ ██╔╝██║]],
+  [[     ██║██║   ██║███████╗███████║    ██╔████╔██║█████╗  ██║  ██║█████╗  ███████╗█████╔╝ ██║]],
+  [[██   ██║██║   ██║╚════██║██╔══██║    ██║╚██╔╝██║██╔══╝  ██║  ██║██╔══╝  ╚════██║██╔═██╗ ██║]],
+  [[╚█████╔╝╚██████╔╝███████║██║  ██║    ██║ ╚═╝ ██║███████╗██████╔╝███████╗███████║██║  ██╗██║]],
+  [[ ╚════╝  ╚═════╝ ╚══════╝╚═╝  ╚═╝    ╚═╝     ╚═╝╚══════╝╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝╚═╝]]
 }
 dashboard.section.buttons.val = {
-  dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-  dashboard.button("e", " " .. " New file", ":ene <BAR> startinsert <CR>"),
-  dashboard.button("p", " " .. " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
+  dashboard.button("p", " " .. " Find file", ":Telescope git_files <CR>"),
+  dashboard.button("h", " " .. " Harpoon Marks", ":Telescope harpoon marks <CR>"),
   dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
   dashboard.button("t", " " .. " Find text", ":Telescope live_grep <CR>"),
   dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
   dashboard.button("q", " " .. " Quit", ":qa<CR>"),
 }
 local function footer()
-  return "chrisatmachine.com"
+  return "joshmedeski.com @joshmedeski"
 end
 
 dashboard.section.footer.val = footer()
