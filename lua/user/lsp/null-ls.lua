ce8efb727ace396513790014fa7b5455a3b4dc0f
Josh Medeski <josh.medeski@gmail.com>
2022-11-26 00:10:51 -0600

    feat: add astro

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -1,3 +1,4 @@
+-- https://github.com/jose-elias-alvarez/null-ls.nvim
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
   return
@@ -13,7 +14,8 @@ null_ls.setup {
   debug = false,
   sources = {
     formatting.prettier.with {
-      extra_filetypes = { "toml" },
+      -- TODO: test that astro works
+      extra_filetypes = { "toml", "astro" },
       extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
     },
     formatting.black.with { extra_args = { "--fast" } },
