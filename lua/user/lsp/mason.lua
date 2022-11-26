a2bd83061c58b03fb288b7ab2006632b76de4875
Josh Medeski <josh.medeski@gmail.com>
2022-11-26 00:04:13 -0600

    feat: update mason icons

--- lua/user/lsp/mason.lua
+++ lua/user/lsp/mason.lua
@@ -11,12 +11,11 @@ local servers = {
 
 local settings = {
 	ui = {
-		border = "none",
-		icons = {
-			package_installed = "◍",
-			package_pending = "◍",
-			package_uninstalled = "◍",
-		},
+    icons = {
+      package_installed = "﫟",
+      package_pending = "﫠",
+      package_uninstalled = ""
+    }
 	},
 	log_level = vim.log.levels.INFO,
 	max_concurrent_installers = 4,
