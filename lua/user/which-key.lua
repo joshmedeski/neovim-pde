3ad891f5ff4727f08c6832ca519618ec116f73cd
Josh Medeski <josh.medeski@gmail.com>
2022-11-26 01:14:13 -0600

    feat: add more which keys

--- lua/user/which-key.lua
+++ lua/user/which-key.lua
@@ -3,7 +3,7 @@ local wk = require("which-key")
 wk.setup { }
 
 wk.register({
-  ["<leader>"] = {"<cmd>lua require('harpoon.mark').add_file()<CR>", "Add to Harpoon"},
+  ["'"] = {"<cmd>lua require('harpoon.mark').add_file()<CR>", "Add to Harpoon"},
   ["0"] = {"<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", "Harpoon Menu"},
   ["1"] = {"<cmd>lua require('harpoon.ui').nav_file(1)<CR>", "Harpoon 1"},
   ["2"] = {"<cmd>lua require('harpoon.ui').nav_file(2)<CR>", "Harpoon 2"},
@@ -35,32 +35,30 @@ wk.register({
 
   f = { "<cmd>lua require('lf').start()<cr>", "Lf"},
   ["%"] = { "<cmd>source %<cr>", ":source %"},
-  ["/"] = { "<cmd>noh<cr>", "No search highlight"},
+  ["/"] = { "<cmd>noh<cr>", "Clear find"},
+
 -- nmap <leader><leader> <Plug>(coc-fix-current)
--- nmap <leader><tab> :bp<cr>
+-- nmap <leader>G :Goyo<cr>
 -- nmap <leader>a <Plug>(coc-codeaction)
 -- nmap <leader>bsd :%bd\|e#\|bd#<cr>\|'"
--- nmap <leader>bd :bd<cr>
--- nmap <leader>en <Plug>(coc-diagnostic-next)
 -- nmap <leader>ee <Plug>(coc-fix-current)
+-- nmap <leader>en <Plug>(coc-diagnostic-next)
 -- nmap <leader>ep <Plug>(coc-diagnostic-prev)
--- nmap <leader>f :Lfcd<cr>
+-- nmap <leader>gP :CocCommand git.push<cr>
+-- nmap <leader>gU :CocCommand git.chunkUnstage<cr>
 -- nmap <leader>gb <Plug>(coc-git-blame)<cr>
 -- nmap <leader>gc :CocCommand git.showCommit<cr>
 -- nmap <leader>gdc :CocCommand git.diffCached<cr>
 -- nmap <leader>gen <Plug>(coc-git-prevconflict)<cr>
 -- nmap <leader>gep <Plug>(coc-git-nextconflict)<cr>
--- nmap <leader>G :Goyo<cr>
 -- nmap <leader>gg :CocCommand git.chunkStage<cr>
 -- nmap <leader>gi :CocCommand git.chunkInfo<cr>
 -- nmap <leader>gn <Plug>(coc-git-nextchunk)<cr>
 -- nmap <leader>go :CocCommand git.browserOpen<cr>
--- nmap <leader>gP :CocCommand git.push<cr>
 -- nmap <leader>gp <Plug>(coc-git-prevchunk)<cr>
 -- nmap <leader>gs :GitStatus<cr>
 -- nmap <leader>gt :CocCommand git.toggleGutters<cr>
 -- nmap <leader>gu :CocCommand git.chunkUndo<cr>
--- nmap <leader>gU :CocCommand git.chunkUnstage<cr>
 -- nmap <leader>gy :CocCommand git.copyUrl<cr><cr>
 -- nmap <leader>gz :CocCommand git.foldUnchanged<cr>
 -- nmap <leader>h :HopWord<cr>
@@ -68,17 +66,10 @@ wk.register({
 -- nmap <leader>k :<C-u>CocPrev<CR>
 -- nmap <leader>l :HopLine<cr>
 -- nmap <leader>m :MaximizerToggle!<cr>
--- nmap <leader>n :bn<cr>
 -- nmap <leader>o :<C-u>CocList outline<cr>
--- nmap <leader>p :bp<cr>
 -- nmap <leader>q :wq!<cr>
 -- nmap <leader>rn <Plug>(coc-rename)
 -- nmap <leader>s :<C-u>CocList -I symbols<cr>
 --
 -- nnoremap <leader>\ <cmd>lua require('telescope.builtin').live_grep()<cr>
--- nnoremap <leader>bb <cmd>lua require('telescope.builtin').buffers()<cr>
--- omap ag <Plug>(coc-git-chunk-outer)
--- omap ig <Plug>(coc-git-chunk-inner)
--- xmap ag <Plug>(coc-git-chunk-outer)
--- xmap ig <Plug>(coc-git-chunk-inner)
 }, { prefix = "<leader>" })
